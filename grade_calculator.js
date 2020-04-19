8699f5c4ac1f41d74eceb17138952ba8720a269b
TaronAndranyan <62055527+TaronAndranyan@users.noreply.github.com>
2020-04-19 23:17:08 +0400

    Update grade_calculator.js

--- grade_calculator.js
+++ grade_calculator.js
@@ -1,9 +1,21 @@
-function grade_calculator(){
-  var partgrade = document.getElementById("part_grade")
-  var homeworkgrade = document.getElementById("homework_grade")
-  var prmil1 = document.getElementById("prmil1")
-  var prmil2 = document.getElementById("prmil2")
-  var prmil3 = document.getElementById("prmil3")
-  var final grade = document.getElementById("mycurrentgrade").innerHTML = 10% * partgrade + 20% * homeworkgrade + 20% * prmil1 + 20% * prmil2 + 30% * prmil3
-  return mycurrentgrade
-}
+function GradeCalculator()
+        {
+            var ParticipationPercentage =parseInt(document.getElementById('PartPercent').innerHTML);
+            var HomeworkPercentage=parseInt(document.getElementById('hwpercent').innerHTML);
+            var PM1Percentage=parseInt(document.getElementById('pmpercent').innerHTML);
+            var PM2Percentage=parseInt(document.getElementById("pm2percent").innerHTML);
+            var PM3Percentage=parseInt(document.getElementById('pm3percent').innerHTML);
+
+
+            var ParticipationGrade =parseFloat(document.getElementById('part_grade').value);
+            var HomeworkGrade =parseFloat(document.getElementById('homework_grade').value);
+            var PM1Grade =parseFloat(document.getElementById('prmil1').value);
+            var PM2Grade =parseFloat(document.getElementById('prmil2').value);
+            var PM3Grade =parseFloat(document.getElementById('prmil3').value);
+
+
+            var Mycurrentgrade = ParticipationPercentage*ParticipationGrade/100 + HomeworkPercentage*HomeworkGrade/100 + PM1Percentage*PM1Grade/100 + PM2Percentage*PM2Grade/100 + PM3Percentage*PM3Grade/100
+
+            var display=document.getElementById('mycurrentgrade');
+            display.innerHTML='My current grade is-' +  Mycurrentgrade;
+            }
